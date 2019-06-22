4ca6e932b03647402d262faf3d3d307df6ebb111
Armando Couto <coutoarmando@gmail.com>
2019-06-21 23:43:31 -0300

    Corrige routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  mount Rswag::Ui::Engine => '/api-docs'
-  mount Rswag::Api::Engine => '/api-docs'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, param: :_username
   post '/auth/login', to: 'authentication#login'
