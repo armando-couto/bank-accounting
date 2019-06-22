5be70d2bb06e291d51dffdb16f4d6c2cde8ae99b
Armando Couto <coutoarmando@gmail.com>
2019-06-22 10:11:19 -0300

    melhora a documantação e adiciona as configuracoes de API.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, param: :id
   post '/auth/login', to: 'authentication#login'
   get '/*a', to: 'application#not_found'
+
+  namespace 'api' do
+    namespace 'v1' do
+      resources :users
+    end
+  end
 end
